b5c1f3627deda60779c3f4c05df8581c2bea4b8a
Mia5aLifa123xNxx <45575591+Mia5aLifa123xNxx@users.noreply.github.com>
2019-02-21 11:16:27 +0300

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : bearboy`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By :bearboy ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`ana zwin`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login(NTM3MzU2Nzg0MTU5OTQ4ODE2.D0s8cQ.bxoPwKbgldCtm7-kPd6gHwi2Txw);
